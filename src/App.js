ccf16f992fbfe767a30396151ccb0f08649eec01
SlimDridi01 <slimgotze01@gmail.com>
2021-11-09 13:29:52 +0000

    first react

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Form1 from './Form/Form1'
+
+function App() {
+  return (
+    <div className="App">
+      <Form1 />
+    </div>
+  );
+}
+
+export default App;
